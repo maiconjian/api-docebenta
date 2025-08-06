abd73283119df4c14ee99c648c1b1c815c48d3a8
maiconjian <maiconjiantelles@gmail.com>
2025-08-06 19:20:52 -0300

    commit inicial

---
+++ src/main/java/com/doce/benta/DocebentaApplication.java
@@ -0,0 +1,13 @@
+package com.doce.benta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocebentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocebentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doce/benta/DocebentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doce.benta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocebentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
